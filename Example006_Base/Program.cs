b27fdd2ce2d060e1bf89973c82acd771b1dacf48
arvsokolov <marktwe@mail.ru>
2023-05-13 02:25:02 +0300

    Вычисляю максимальное число.

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 4;
+int b = 8;
+int c = 12;
+int d = 16;
+int e = 20;
+int f = 24;
+
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+if(f > max) max = f;
+
+Console. WriteLine("Максимальное число = " + max);
