9ae3b626b2b0becf9764b67a1d2b7af9229f4730
arvsokolov <marktwe@mail.ru>
2023-05-13 01:52:22 +0300

    if- else

---
+++ Example005_Name/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string name = Console.ReadLine();
+    if(name.ToLower() == "маша"){
+        Console.WriteLine("Ура, это же Маша!");
+    } else{
+        Console.WriteLine("Привет, " + name);
+    }
